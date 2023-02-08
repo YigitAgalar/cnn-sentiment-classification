86c3b67736949f10035f8a19d3b7521ec85dc6ca
Yiğit Ağalar <45517321+YigitAgalar@users.noreply.github.com>
2023-02-08 12:01:23 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,80 @@
+#streamlit application that displays the results of the model
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+
+st.set_page_config(
+    page_title="streamlit_main",
+    page_icon="👋",
+)
+
+
+
+
+
+st.title('Sentiment analizlere göre yapılan Time Series çalışmasının en iyi metriklere sahip modellerinin karşılaştırılması')
+
+
+allscores = pd.read_csv("data/all_results.csv",index_col=0)
+st.subheader('Time Series Analysis')
+
+st.subheader('Kullanılan 3 farklı modelin özet bilgileri')
+
+st.subheader('model 1 : LSTM')
+st.code(''' rbct_lstm = Sequential()
+rbct_lstm.add(LSTM(64, return_sequences=True, input_shape = (trainX.shape[1],trainX.shape[2])))
+
+rbct_lstm.add(LSTM(64,return_sequences=False))
+rbct_lstm.add(Dense(25))
+rbct_lstm.add(Dense(1))
+
+
+rbct_lstm.summary()
+   
+''')
+
+st.subheader('model 2 : Conv1D')
+st.code(''' 
+rbct_conv1d = Sequential()
+rbct_conv1d.add(InputLayer((trainX.shape[1],trainX.shape[2])))
+rbct_conv1d.add(Conv1D(64, kernel_size=2))
+rbct_conv1d.add(Flatten())
+rbct_conv1d.add(Dense(8, 'linear'))
+rbct_conv1d.add(Dense(1, 'linear'))
+
+rbct_conv1d.summary()
+   
+''')
+
+st.subheader('model 3: Conv1D + LSTM')
+st.code(''' rbct_convlstm = Sequential()
+rbct_convlstm.add(InputLayer((trainX.shape[1],trainX.shape[2])))
+rbct_convlstm.add(Conv1D(64, kernel_size=2))
+rbct_convlstm.add(LSTM(64,return_sequences=False))
+rbct_convlstm.add(Dense(8, 'linear'))
+rbct_convlstm.add(Dense(1, 'linear'))
+
+rbct_convlstm.summary()
+     
+   
+''')
+
+
+st.subheader('En iyi çalışan 2 model')
+
+st.write(allscores[allscores["r2"]==allscores["r2"].max()])
+
+
+st.write(allscores[(allscores["mae"]==allscores["mae"].min())])
+
+
+
+
+st.subheader('En iyi 2 modelin tahmin grafikleri')
+st.image('img/fbct_hyb_ts_pred.png')
+st.image('img/rbct_cnn_ts_pred.png')
---
+++ pages/dbct.py
@@ -0,0 +1,77 @@
+#streamlit application that displays the results of the model
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras.utils import to_categorical
+from sklearn.metrics import accuracy_score,f1_score,precision_score,recall_score
+
+
+sentiments = pd.read_csv("sentiments.csv")
+df_tfidf_clean = pd.read_csv("data/distilbert_clean.csv",index_col=0)
+
+
+@st.cache
+# fonksiyonun çalışması için csvlerdeki column isimleriyle eşleşen name değişkeni fonksiyon çağrılırken verildi
+def max_score_extraction(df,name):
+    for column in df.columns:
+        pd.to_numeric(df[column])
+    df[f"{name}_max_score"]=df.max(axis=1)
+    df[f"{name}_max_score_column"]=df.idxmax(axis=1)
+    df["sentiment"] = df.max(axis=1)
+    for value in range(0,len(df[f"{name}_max_score_column"])):
+        if df[f"{name}_max_score_column"][value] == df.columns[2]:
+            pass
+        elif df[f"{name}_max_score_column"][value] == df.columns[0]:
+            df[f"{name}_max_score"][value] = df[f"{name}_max_score"][value]* -1
+        elif df[f"{name}_max_score_column"][value] == df.columns[1]:
+            df[f"{name}_max_score"][value] = 0
+    for index in range(0,len(df[f"{name}_max_score_column"])):
+        if df[f"{name}_max_score"][index] > 0.05:
+            df["sentiment"][index]= int(1)
+        elif df[f"{name}_max_score"][index] < -0.05:
+            df["sentiment"][index]= int(-1)
+        elif df[f"{name}_max_score"][index] >= -0.05 and df[f"{name}_max_score"][index] <= 0.05:
+            df["sentiment"][index]= int(0)
+    df["sentiment"]=pd.to_numeric(df["sentiment"],downcast='integer')
+    return df
+
+
+
+df_tfidf_clean = max_score_extraction(df_tfidf_clean,"distilbert_clean")
+st.title('Bitcoin hakkında atılan tweetlerin sentiment analizi')
+
+st.subheader('distilbert Transformer ile Sentiment Analizi')
+
+
+st.subheader('Duyguların dağılımı -1 negatif, 0 nötr, 1 pozitif')
+st.write(sentiments["sentiment"].value_counts(normalize=True))
+#make a barplot
+st.bar_chart(sentiments["sentiment"].value_counts(normalize=True))
+#what are the columns in the dataset
+st.subheader('Veri Seti Özellikleri')
+st.write(df_tfidf_clean.columns)
+#show the dataset
+st.write(df_tfidf_clean[["distilbert_neg_clean","distilbertdistilbert_neu_clean","distilbert_pos_clean","distilbert_clean_max_score","sentiment"]].sample(10))
+
+st.subheader('Modelin Performansı')
+#show the model performance
+st.write('Accuracy Score:',round(accuracy_score(sentiments["sentiment"],df_tfidf_clean["sentiment"]),2))
+st.write('Precision Score:',round(precision_score(sentiments["sentiment"],df_tfidf_clean["sentiment"],average='weighted'),2))
+st.write('Recall Score:',round(recall_score(sentiments["sentiment"],df_tfidf_clean["sentiment"],average='weighted'),2))
+st.write('F1 Score:',round(f1_score(sentiments["sentiment"],df_tfidf_clean["sentiment"],average='weighted'),2))
+
+
+
+allscores = pd.read_csv("data/all_results.csv",index_col=0)
+st.subheader('Time Series Analysis')
+#select row from allscores dataframe that starts with tfct 
+#show row from allscores dataframe that starts with tfct
+st.dataframe(allscores[allscores.index.str.startswith("dbct")])
+
+
+st.image('img/dbct_lstm_ts_pred.png')
+st.image('img/dbct_cnn_ts_pred.png')
+st.image('img/dbct_hyb_ts_pred.png')
